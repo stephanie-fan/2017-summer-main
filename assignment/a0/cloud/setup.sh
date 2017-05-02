dc74db5c67e79d24512cc2523f9cb0b83ef5049f
jameskunz <james@kunz.ca>
2017-05-01 22:01:45 -0700

    Tweak TF URL.

--- assignment/a0/cloud/setup.sh
+++ assignment/a0/cloud/setup.sh
@@ -15,7 +15,7 @@ bash "$ANACONDA_INSTALLER"
 
 source ${HOME}/.bashrc
 
-TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.1.0-cp34-cp34m-linux_x86_64.whl
+TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.1.0-cp27-none-linux_x86_64.whl
 ${HOME}/anaconda2/bin/pip install --upgrade pip
 ${HOME}/anaconda2/bin/pip install $TF_BINARY_URL
 ${HOME}/anaconda2/bin/jupyter notebook --generate-config
