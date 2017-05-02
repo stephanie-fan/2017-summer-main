ac84bb40f37f36d29d915acaacd001d123491d46
jameskunz <james@kunz.ca>
2017-05-01 21:46:01 -0700

    Updated setup with new software versions.

--- assignment/a0/cloud/setup.sh
+++ assignment/a0/cloud/setup.sh
@@ -9,13 +9,13 @@ pushd ${HOME}
 sudo apt-get update
 sudo apt-get upgrade
 
-ANACONDA_INSTALLER="Anaconda2-4.2.0-Linux-x86_64.sh"
+ANACONDA_INSTALLER="Anaconda2-4.3.1-Linux-x86_64.sh"
 wget "https://repo.continuum.io/archive/$ANACONDA_INSTALLER"
 bash "$ANACONDA_INSTALLER"
 
 source ${HOME}/.bashrc
 
-TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.12.1-cp27-none-linux_x86_64.whl
+TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.1.0-cp34-cp34m-linux_x86_64.whl
 ${HOME}/anaconda2/bin/pip install --upgrade pip
 ${HOME}/anaconda2/bin/pip install $TF_BINARY_URL
 ${HOME}/anaconda2/bin/jupyter notebook --generate-config
