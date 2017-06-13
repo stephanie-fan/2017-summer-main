db2711a85de3bed79fd1847aaed4094a28437f92
jameskunz <james@kunz.ca>
2017-06-13 00:19:47 -0700

    Release A3, and week 4 materials.

--- assignment/submit.sh
+++ assignment/submit.sh
@@ -12,7 +12,7 @@
 #   -a <number> : specify assignment number
 
 GITHUB_USERNAME="${USER}"
-DEFAULT_ASSIGNMENT="0"
+DEFAULT_ASSIGNMENT="3"
 FORCE="false"
 TARGET_BRANCH="master"
 
@@ -21,6 +21,7 @@ function validate_assignment() {
 0
 1
 2
+3
 EOF
 }
 
